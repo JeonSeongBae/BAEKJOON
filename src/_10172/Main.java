7f54cf6447a0d7a8d309e1fe6cca046fe03aa9d5
JeonSeongBae <tjdqo6042@naver.com>
2018-03-20 17:16:29 +0900

    image print

---
+++ src/_10172/Main.java
@@ -0,0 +1,11 @@
+package _10172;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\r");
+		System.out.println("|q p|   /}");
+		System.out.println("|\\_/|\r\n");
+		System.out.println("|\\_/|\r\n");
+		System.out.println("|\\_/|\r\n");
+	}
+}

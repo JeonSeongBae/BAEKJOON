34a4fdadf51376555295a3b850c902507f335638
JeonSeongBae <tjdqo6042@naver.com>
2018-03-21 15:46:51 +0900

    revising comment

--- src/_11719/Main.java
+++ src/_11719/Main.java
@@ -3,7 +3,7 @@ package _11719;
 import java.util.Scanner;
 
 public class Main {
-
+	// 입력받은 문자를 출력하는 문제2
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNextLine()) {
--- src/_2557/Main.java
+++ src/_2557/Main.java
@@ -2,6 +2,7 @@ package _2557;
 
 public class Main {
 	public static void main(String[] args) {
+		// Hello World! 를 화면에 출력하는 문제
 		System.out.print("Hello World!");
 	}
 }

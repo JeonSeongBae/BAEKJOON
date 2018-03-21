0084a0877731f406ef9a443fdb76b0db3fe967d1
JeonSeongBae <tjdqo6042@naver.com>
2018-03-21 15:45:01 +0900

    모든 사칙연산 해보기

---
+++ src/_10869/Main.java
@@ -0,0 +1,18 @@
+package _10869;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 모든 사칙연산 해보기
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+	}
+}

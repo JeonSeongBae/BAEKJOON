8529880b28794dbf0fbdeee2eede5087d48c8d1a
JeonSeongBae <tjdqo6042@naver.com>
2018-03-21 15:46:17 +0900

    나머지 연산을 하고 계산한 결과와 계산을 한 결과를 나머지 연산한 결과가 같은지를 살펴봅시다.

---
+++ src/_10430/Main.java
@@ -0,0 +1,18 @@
+package _10430;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 나머지 연산을 하고 계산한 결과와 계산을 한 결과를 나머지 연산한 결과가 같은지를 살펴봅니다
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		System.out.println((A + B) % C);
+		System.out.println((A % C + B % C) % C);
+		System.out.println((A * B) % C);
+		System.out.println((A % C * B % C) % C);
+	}
+}

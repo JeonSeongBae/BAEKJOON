c534b0cf7f065245ee6beb1204357dbd1bad1c84
SeonJae <dbtjswo4733@naver.com>
2018-03-22 18:11:57 +0900

    testing

--- src/_1000/Main.java
+++ src/_1000/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// 두 수를 입력받고 합을 출력하는 문제
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		
 		int input = scanner.nextInt();
 		int input2 = scanner.nextInt();
 		System.out.println(input + input2);

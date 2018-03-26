5bd0afff3dc59b63caba7880b6f2e54116263874
JeonSeongBae <tjdqo6042@naver.com>
2018-03-26 16:24:25 +0900

    substring index for summarize

--- src/_2742/Main.java
+++ src/_2742/Main.java
@@ -7,9 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int N = scanner.nextInt();
-
 		for (int i = 0; i < N; i++) {
-			System.out.println(N-i);
+			System.out.println(N - i);
 		}
 	}
 }

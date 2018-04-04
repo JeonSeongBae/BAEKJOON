9b11049ca40698e76fcce7df6edd77c6f6e6da07
JeonSeongBae <tjdqo6042@naver.com>
2018-04-04 22:24:07 +0900

    _2248_print stars

---
+++ src/_2248/Main.java
@@ -0,0 +1,40 @@
+package _2248;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		String[] map;
+		map = new String[n];
+		map[0] = "  *  ";
+		map[1] = " * * ";
+		map[2] = "*****";
+
+		for (int i = 1; 3 * (int) Math.pow(2, i) <= n; i++) {
+			int border = 3 * (int) Math.pow(2, i);
+			int half = border / 2;
+
+			// 아래의 삼각형을 만들어준다.
+			for (int j = border - 1; j >= half; j--) {
+				map[j] = map[j - half] + " " + map[j - half];
+			}
+
+			// 위의 삼각형을 밀어준다
+			String space = "";
+			while (space.length() < half) {
+				space += " ";
+			}
+			for (int j = half - 1; j >= 0; j--) {
+				map[j] = space + map[j] + space;
+			}
+		}
+
+		// print
+		for (int i = 0; i < n; i++) {
+			System.out.println(map[i]);
+		}
+	}
+}

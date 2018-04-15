a4baf6824d8eb66cb7fb3fc89e9a934753e8019a
JeonSeongBae <tjdqo6042@naver.com>
2018-04-15 22:59:20 +0900

    make class and bufferedReader

---
+++ src/_1316.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1316 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		for (int i = 0; i < size; i++) {
+			String input = br.readLine();
+			boolean[] exist = new boolean[26];
+			for (int j = 0; j < input.length(); j++) {
+				
+			}
+		}
+	}
+}

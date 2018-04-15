4df9d26580ef0eef1bce95e4aecdad3e6a55471f
JeonSeongBae <tjdqo6042@naver.com>
2018-04-15 22:59:07 +0900

    modifed comment

--- src/_5622/Main.java
+++ src/_5622/Main.java
@@ -42,32 +42,39 @@ public class Main {
 		case 'E':
 		case 'F':
 			return 4;
+
 		case 'G':
 		case 'H':
 		case 'I':
 			return 5;
+		
 		case 'J':
 		case 'K':
 		case 'L':
 			return 6;
+		
 		case 'M':
 		case 'N':
 		case 'O':
 			return 7;
+		
 		case 'P':
 		case 'Q':
 		case 'R':
 		case 'S':
 			return 8;
+		
 		case 'T':
 		case 'U':
 		case 'V':
 			return 9;
+		
 		case 'W':
 		case 'X':
 		case 'Y':
 		case 'Z':
 			return 10;
+		
 		default:
 			return 0;
 		}

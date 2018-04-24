122acd0c3766d2b7ac62eee10746ee2c19628832
JeonSeongBae <tjdqo6042@naver.com>
2018-04-24 22:01:42 +0900

    error_ under 14

--- src/_1193/Main.java
+++ src/_1193/Main.java
@@ -6,34 +6,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
+		int 입력값 = sc.nextInt();
 		sc.close();
 
-		boolean odd = oddNumber(input);
-		int sum = 0;
-		int up = 0;
-		int down = 1;
-		while (input > sum) {
-			up++;
-			sum += up;
+		boolean isOdd = oddNumber(입력값);
+		int tempSum = 0;
+		int 분모 = 0;
+		int 분자 = 1;
+		while (입력값 > tempSum) {
+			분모++;
+			tempSum += 분모;
 		}
-		sum = input - (sum - up) - 1;
+		tempSum = 입력값 - (tempSum - 분모) - 1;
 
-		if (odd) {
+		if (isOdd) {
 			// 홀수이면 분모가 -- 분자가 ++
-			for (int i = 0; i < sum; i++) {
-				up++;
-				down--;
+			for (int i = 1; i < tempSum; i++) {
+				분모++;
+				분자--;
 			}
 		} else {
 			// 짝수이면 분모가 ++ 분자가 --
-			for (int i = 0; i < sum; i++) {
-				up--;
-				down++;
+			for (int i = 1; i < tempSum; i++) {
+				분모--;
+				분자++;
 			}
 		}
-		System.out.println(up);
-		System.out.println(down);
+		System.out.println(분모 + "/" + 분자);
 	}
 
 	private static boolean oddNumber(int input) {

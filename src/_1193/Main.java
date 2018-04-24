cad76ef3d6ebf5b038b764b112e547f722b5225a
JeonSeongBae <tjdqo6042@naver.com>
2018-04-24 22:18:54 +0900

    remove 분모, 분자 ++--

--- src/_1193/Main.java
+++ src/_1193/Main.java
@@ -5,35 +5,24 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		while (true) {
-			Scanner sc = new Scanner(System.in);
-			int 입력값 = sc.nextInt();
-
-			boolean isOdd = oddNumber(입력값);
-			int tempSum = 0;
-			int 분모 = 0;
-			int 분자 = 1;
-			while (입력값 >= tempSum) {
-				분모++;
-				tempSum += 분모;
-			}
-			tempSum = 입력값 - (tempSum - 분모);
-
-			if (isOdd) {
-				// 홀수이면 분모가 -- 분자가 ++
-				for (int i = 0; i < tempSum; i++) {
-					분모++;
-					분자--;
-				}
-			} else {
-				// 짝수이면 분모가 ++ 분자가 --
-				for (int i = 0; i < tempSum; i++) {
-					분모--;
-					분자++;
-				}
-			}
-			System.out.println(분모 + "/" + 분자);
+		Scanner sc = new Scanner(System.in);
+		int 입력값 = sc.nextInt();
+		sc.close();
+		boolean isOdd = oddNumber(입력값);
+		int tempSum = 0;
+		int 분모 = 1;
+		int 분자 = 1;
+		while (입력값 >= tempSum) {
+			분모++;
+			tempSum += 분모;
 		}
+
+		if (isOdd) {
+			// 홀수일경우
+		} else {
+			// 짝수일경우
+		}
+		System.out.println(분자 + "/" + 분모);
 	}
 
 	private static boolean oddNumber(int input) {

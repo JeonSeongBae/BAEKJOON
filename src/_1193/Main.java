fdea98ec876fd8335179477cd8c21d295b5f5703
JeonSeongBae <tjdqo6042@naver.com>
2018-04-24 22:13:12 +0900

    error need to revise

--- src/_1193/Main.java
+++ src/_1193/Main.java
@@ -5,34 +5,35 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int 입력값 = sc.nextInt();
-		sc.close();
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			int 입력값 = sc.nextInt();
 
-		boolean isOdd = oddNumber(입력값);
-		int tempSum = 0;
-		int 분모 = 0;
-		int 분자 = 1;
-		while (입력값 > tempSum) {
-			분모++;
-			tempSum += 분모;
-		}
-		tempSum = 입력값 - (tempSum - 분모) - 1;
-
-		if (isOdd) {
-			// 홀수이면 분모가 -- 분자가 ++
-			for (int i = 1; i < tempSum; i++) {
+			boolean isOdd = oddNumber(입력값);
+			int tempSum = 0;
+			int 분모 = 0;
+			int 분자 = 1;
+			while (입력값 >= tempSum) {
 				분모++;
-				분자--;
+				tempSum += 분모;
 			}
-		} else {
-			// 짝수이면 분모가 ++ 분자가 --
-			for (int i = 1; i < tempSum; i++) {
-				분모--;
-				분자++;
+			tempSum = 입력값 - (tempSum - 분모);
+
+			if (isOdd) {
+				// 홀수이면 분모가 -- 분자가 ++
+				for (int i = 0; i < tempSum; i++) {
+					분모++;
+					분자--;
+				}
+			} else {
+				// 짝수이면 분모가 ++ 분자가 --
+				for (int i = 0; i < tempSum; i++) {
+					분모--;
+					분자++;
+				}
 			}
+			System.out.println(분모 + "/" + 분자);
 		}
-		System.out.println(분모 + "/" + 분자);
 	}
 
 	private static boolean oddNumber(int input) {

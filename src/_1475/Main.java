f0885d0daeee8d6fec01facf3da814dfc0c4f681
JeonSeongBae <tjdqo6042@naver.com>
2018-05-07 23:33:42 +0900

    Conuting Number's member

--- src/_1475/Main.java
+++ src/_1475/Main.java
@@ -13,8 +13,12 @@ public class Main {
 		 * 때, 필요한 세트의 개수의 최소값을 출력하시오. (6은 9를 뒤집어서 이용할 수 있고, 9는 6을 뒤집어서 이용할 수 있다.)
 		 */
 		Scanner sc = new Scanner(System.in);
-		int input = sc.nextInt();
+		char[] input = sc.next().toCharArray();
+		int[] number = new int[10];
 		sc.close();
+		for (int i = 0; i < input.length; i++) {
+			number[input[i] - 48]++;
+		}
 	}
 
 }

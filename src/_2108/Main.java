e08d547eb803630bcd5e4d8801e4861fdf3f1226
JeonSeongBae <tjdqo6042@naver.com>
2018-05-19 23:22:37 +0900

    2108_comment

--- src/_2108/Main.java
+++ src/_2108/Main.java
@@ -4,8 +4,15 @@ import java.util.*;
 import java.util.Arrays;
 
 public class Main {
-
 	public static void main(String[] args) {
+		/*
+		 * 수를 처리하는 것은 통계학에서 상당히 중요한 일이다. 통계학에서 N개의 수를 대표하는 기본 통계값에는 다음과 같은 것들이 있다. 단, N은
+		 * 홀수라고 가정하자.
+		 * 
+		 * 산술평균 : N개의 수들의 합을 N으로 나눈 값 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값 최빈값 :
+		 * N개의 수들 중 가장 많이 나타나는 값 범위 : N개의 수들 중 최대값과 최소값의 차이 N개의 수가 주어졌을 때, 네 가지 기본 통계값을
+		 * 구하는 프로그램을 작성하시오.
+		 */
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt(); // 반복횟수
 		int[] array = new int[size]; // 입력받는 정수

a566354db804d262a9488a20d7d111db57152483
JeonSeongBae <tjdqo6042@naver.com>
2018-05-27 21:06:19 +0900

    scanner.close

--- src/_2741/Main.java
+++ src/_2741/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int N = scanner.nextInt();
-
+		scanner.close();
 		for (int i = 1; i <= N; i++) {
 			System.out.println(i);
 		}

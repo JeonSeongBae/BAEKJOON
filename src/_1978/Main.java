87af41eb237e676e5d9f3566648d3d83e778b9b3
JeonSeongBae <tjdqo6042@naver.com>
2018-05-30 21:13:50 +0900

    add pakage

---
+++ src/_1978/Main.java
@@ -0,0 +1,12 @@
+package _1978;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+		Scanner scanner = new Scanner(System.in);
+		scanner.close();
+	}
+}
